44d81faf2be19a44388d868c41c6f2c9c6031b14
Christopher Chudzicki <cchudzicki@Christophers-MacBook-Pro.local>
2021-11-25 16:07:07 -0500

    setup eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  rules: {
+    "no-console": "error",
+    "import/first": "error",
+    "react/prop-types": "off",
+  },
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  root: true,
+  plugins: ["react", "@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: 11,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      pragma: "React",
+      version: "detect",
+    },
+  },
+};
