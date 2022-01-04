4de89a7d38799d267d2f701042a2b9df3b89730d
Christopher Chudzicki <christopher.chudzicki@gmail.com>
2022-01-03 22:07:04 -0500

    add Req/Res types & move ajv validation

--- server/src/controllers/user/signup.ts
+++ server/src/controllers/user/signup.ts
@@ -1,10 +1,16 @@
-import Ajv, { Schema } from "ajv";
+import { Schema } from "ajv";
 import type { Request, Response } from "express";
 import { sendEmail } from "../../util/email";
-import { ClientError } from "../../util/errors";
+import * as ajv from "../../util/ajv";
 import { User } from "../../database/models";
 
-const ajv = new Ajv();
+type PathParams = never;
+type BodyParams = { email: string };
+type QueryParams = never;
+type ResponseBody = { result: true };
+
+type SignupRequest = Request<PathParams, ResponseBody, BodyParams, QueryParams>;
+type SignupResponse = Response<ResponseBody>;
 
 const schema: Schema = {
   type: "object",
@@ -14,20 +20,18 @@ const schema: Schema = {
   required: ["email"],
   additionalProperties: false,
 };
-const validate = ajv.compile(schema);
 
-const validateRequest = (req: Request) => {
-  const isValid = validate(req.body);
-  if (!isValid) {
-    const errMsg = validate.errors?.[0]?.message ?? "Error";
-    throw new ClientError(errMsg);
-  }
-  return true;
-};
+const validateBody = ajv.makeValidator<BodyParams>(schema);
 
-const signup = async (req: Request, res: Response): Promise<void> => {
+const validateRequest: (req: Request) => asserts req is SignupRequest = (
+  req: Request
+) =>
+  // TODO: validate email
+  validateBody(req.body);
+
+const signup = async (req: Request, res: SignupResponse): Promise<void> => {
   validateRequest(req);
-  const { email }: { email: string } = req.body;
+  const { email } = req.body;
   const user = await User.findByEmail(email);
   if (user) {
     await sendEmail(email, "existingUserEmail");
