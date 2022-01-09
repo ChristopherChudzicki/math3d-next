8ca2e1d0e1f7352b098510608d107990fe4bbd33
Christopher Chudzicki <christopher.chudzicki@gmail.com>
2022-01-09 15:14:09 -0500

    add token type

--- server/src/util/auth.ts
+++ server/src/util/auth.ts
@@ -4,9 +4,6 @@ import { ClientError } from "./errors";
 
 const JWT_SECRET = getEnvVar("JWT_SECRET");
 const JWT_EXPIRATION = "30 min";
-const JWT_SECRET_ACCESS = `access:${JWT_SECRET}`;
-const JWT_SECRET_SIGNUP = `signup:${JWT_SECRET}`;
-
 /**
  * For an authHeader string of form "Bearer<whitespace><tokenString>",
  * returns just tokenString
@@ -17,22 +14,35 @@ export const parseAuthHeaderForBearer = (authHeader: string): string => {
   return authHeader.substring(scheme.length).trimStart();
 };
 
+type TokenType = "access" | "signup";
+
 type AccessToken = {
+  type: "access";
   userId: string;
   iat: number;
   exp: number;
 };
 
 type SignupToken = {
+  type: "signup";
   email: string;
   iat: number;
   exp: number;
 };
 
-const verify = (token: string, secret: string) => {
+const verify = (token: string, type: TokenType, secret: string) => {
   try {
-    return jwt.verify(token, secret);
+    const decoded = jwt.verify(token, secret);
+    if (typeof decoded === "string") {
+      throw new ClientError("Forbidden", 403);
+    }
+    if (decoded.type !== type) {
+      throw new ClientError("Forbidden", 403);
+    }
+    return decoded;
   } catch (error) {
+    if (error instanceof ClientError) throw error;
+
     if (error instanceof jwt.TokenExpiredError) {
       throw new ClientError("Token expired", 401);
     }
@@ -51,26 +61,26 @@ const verify = (token: string, secret: string) => {
 
 export const accessToken = {
   generate(userId: string): string {
-    const payload = { userId };
-    return jwt.sign(payload, JWT_SECRET_ACCESS, {
+    const payload = { userId, type: "access" };
+    return jwt.sign(payload, JWT_SECRET, {
       expiresIn: JWT_EXPIRATION,
     });
   },
   verify(token: string): AccessToken {
-    const verified = verify(token, JWT_SECRET_ACCESS) as AccessToken;
+    const verified = verify(token, "access", JWT_SECRET) as AccessToken;
     return verified;
   },
 };
 
 export const signupToken = {
   generate(email: string): string {
-    const payload = { email };
-    return jwt.sign(payload, JWT_SECRET_SIGNUP, {
+    const payload = { email, type: "signup" };
+    return jwt.sign(payload, JWT_SECRET, {
       expiresIn: JWT_EXPIRATION,
     });
   },
   verify(token: string): SignupToken {
-    const verified = verify(token, JWT_SECRET_SIGNUP) as SignupToken;
+    const verified = verify(token, "signup", JWT_SECRET) as SignupToken;
     return verified;
   },
   verifyHeader(authHeader: string): SignupToken {
