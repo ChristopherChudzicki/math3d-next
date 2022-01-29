e7ecdb4bd2d6070809bec815101e2bd3efeb019a
Christopher Chudzicki <christopher.chudzicki@gmail.com>
2022-01-29 14:44:22 -0500

    add simple directed graph utility

---
+++ client/src/util/MathScope/DirectedGraph.ts
@@ -0,0 +1,70 @@
+type Edge<T> = {
+  from: T;
+  to: T;
+};
+
+type DFSCallback<T> = (node: T, depth: number, graph: DirectedGraph<T>) => void;
+
+export default class DirectedGraph<T> {
+  private successors: Map<T, Set<T>>;
+
+  constructor(edges: Edge<T>[]) {
+    this.addEdge = this.addEdge.bind(this);
+    this.dfs = this.dfs.bind(this);
+    this.getDescendants = this.getDescendants.bind(this);
+    this.getSuccessors = this.getSuccessors.bind(this);
+
+    this.successors = new Map();
+
+    edges.forEach((e) => this.addEdge(e.from, e.to));
+  }
+
+  addEdge(from: T, to: T): void {
+    if (!this.successors.has(from)) {
+      this.successors.set(from, new Set());
+    }
+    this.successors.get(from)?.add(to);
+  }
+
+  getSuccessors(node: T): Set<T> {
+    return this.successors.get(node) ?? new Set();
+  }
+
+  private dfsWithDepth(
+    current: T,
+    cb: DFSCallback<T>,
+    depth: number,
+    seen: Set<T>
+  ): void {
+    if (seen.has(current)) return;
+    cb(current, depth, this);
+    seen.add(current);
+    this.getSuccessors(current).forEach((successor) => {
+      this.dfsWithDepth(successor, cb, depth + 1, seen);
+    });
+  }
+
+  dfs(start: T, cb: DFSCallback<T>): void {
+    this.dfsWithDepth(start, cb, 0, new Set());
+  }
+
+  getDescendants(node: T): Set<T> {
+    const descendants = new Set<T>();
+    const storeDescendants: DFSCallback<T> = (current: T, depth: number) => {
+      if (depth === 0) return;
+      descendants.add(current);
+    };
+    this.dfs(node, storeDescendants);
+
+    descendants.forEach((descendant) => {
+      if (this.getSuccessors(descendant).has(node)) {
+        descendants.add(node);
+      }
+    });
+    if (this.getSuccessors(node).has(node)) {
+      descendants.add(node);
+    }
+
+    return descendants;
+  }
+}
