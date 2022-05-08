f6b35094cef727763589cac5370d5459dafc079d
Christopher Chudzicki <christopher.chudzicki@gmail.com>
2022-05-08 08:31:01 -0400

    reorganize mathitem forms

---
+++ client/src/features/sceneControls/mathItems/forms/ParametricCurve/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { MathItem, MathItemType as MT } from "types";
+import ItemTemplate from "../../templates/ItemTemplate";
+import { configs } from "../../configs";
+
+type Props = {
+  item: MathItem;
+};
+const ParametricCurve: React.FC<Props> = ({ item }) => (
+  <ItemTemplate item={item} config={configs[MT.ParametricCurve]} />
+);
+
+export default ParametricCurve;
