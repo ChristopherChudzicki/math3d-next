07cf5a95615163d0685802e890714cda47a82eff
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-09-05 13:18:06 -0400

    smaller bundle

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,6 @@ import "antd/dist/antd.min.css";
 import "./index.css";
 
 import { createTheme } from "@mui/material/styles";
-import * as math from "mathjs";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -13,29 +12,33 @@ import { QueryClient } from "@tanstack/react-query";
 import AppRoutes from "./app";
 import { getStore } from "./store/store";
 
+let setup = Promise.resolve();
+
 if (process.env.NODE_ENV === "development") {
   // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
   const { worker } = require("./test_util/msw/browser");
   // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
   const { seedDb } = require("./test_util");
   seedDb.withFixtures();
-  worker.start();
+  setup = worker.start();
 }
 
-window.math = math;
+setup.then(() => {
+  const container = document.getElementById("root");
+  if (!container) {
+    throw new Error("Could not find root container.");
+  }
+  const root = createRoot(container);
 
-const container = document.getElementById("root");
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = createRoot(container!);
+  const store = getStore();
+  const queryClient = new QueryClient();
+  const theme = createTheme();
 
-const store = getStore();
-const queryClient = new QueryClient();
-const theme = createTheme();
-
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppRoutes queryClient={queryClient} store={store} theme={theme} />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <AppRoutes queryClient={queryClient} store={store} theme={theme} />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+});
