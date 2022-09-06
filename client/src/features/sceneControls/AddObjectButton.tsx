520acbf4a241b18fabbaeafea3e0753e8b8331b5
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-09-05 21:37:00 -0400

    use a mui button for adding objects

--- client/src/features/sceneControls/AddObjectButton.tsx
+++ client/src/features/sceneControls/AddObjectButton.tsx
@@ -1,10 +1,12 @@
-import { Button, Dropdown, Menu, MenuProps } from "antd";
-import { addableTypes, mathItemConfigs, MathItemType } from "@/configs";
-import React, { useCallback } from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { addableTypes, mathItemConfigs } from "@/configs";
+import React, { useRef } from "react";
 import { useAppDispatch } from "@/store/hooks";
-import { assertIsMathItemType } from "@/util/predicates";
 import idGenerator from "@/util/idGenerator";
 
+import { useToggle } from "@/util/hooks";
 import { mathItemsSlice } from "./mathItems";
 
 const { actions } = mathItemsSlice;
@@ -13,33 +15,48 @@ type Props = {
   className?: string;
 };
 
-type MenuClickHandler = NonNullable<MenuProps["onClick"]>;
-
 const AddObjectButton: React.FC<Props> = (props) => {
+  const anchor = useRef<HTMLButtonElement | null>(null);
+  const [open, setOpen] = useToggle(false);
+
   const dispatch = useAppDispatch();
-  const handleClick: MenuClickHandler = useCallback(
-    ({ key }) => {
-      assertIsMathItemType<MathItemType>(key);
-      dispatch(actions.addNewItem({ type: key, id: idGenerator.next() }));
-    },
-    [dispatch]
-  );
+
   return (
-    <Dropdown
-      overlay={
-        <Menu
-          onClick={handleClick}
-          items={addableTypes.map((type) => ({
-            label: mathItemConfigs[type].label,
-            key: type,
-          }))}
-        />
-      }
-      trigger={["click"]}
-      className={props.className}
-    >
-      <Button>Add New Object</Button>
-    </Dropdown>
+    <div className={props.className}>
+      <Button
+        variant="outlined"
+        id="basic-button"
+        ref={anchor}
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={setOpen.on}
+      >
+        Add New Object
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchor.current}
+        open={open}
+        onClose={setOpen.off}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        {addableTypes.map((type) => {
+          const { label } = mathItemConfigs[type];
+          const handleClick = () => {
+            dispatch(actions.addNewItem({ type, id: idGenerator.next() }));
+            setOpen.off();
+          };
+          return (
+            <MenuItem key={type} onClick={handleClick}>
+              {label}
+            </MenuItem>
+          );
+        })}
+      </Menu>
+    </div>
   );
 };
 
