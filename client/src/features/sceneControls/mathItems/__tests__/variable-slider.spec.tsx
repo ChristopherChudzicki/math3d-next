608af67389f4b7db44983424339596d960905d3e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-10-03 20:56:34 -0400

    use fake timers

--- client/src/features/sceneControls/mathItems/__tests__/variable-slider.spec.tsx
+++ client/src/features/sceneControls/mathItems/__tests__/variable-slider.spec.tsx
@@ -158,6 +158,14 @@ const expectSliderArraysEqual = (
 };
 
 describe("Variable Slider", () => {
+  beforeEach(() => {
+    vi.useFakeTimers({ shouldAdvanceTime: true });
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
   test.each([
     {
       duration: 0.6,
@@ -203,12 +211,14 @@ describe("Variable Slider", () => {
       expect(valueUpdates.length).toBe(0);
 
       await user.click(el.btnToggle);
-      await userWaits(periods * duration * 1000);
+
+      await userWaits(periods * duration * 1000, { useFake: true });
+
       await user.click(el.btnToggle);
       expectSliderArraysEqual(valueUpdates, expectedValues);
 
       // No new updates after paused.
-      await userWaits(duration * 1000);
+      await userWaits(duration * 1000, { useFake: true });
       expectSliderArraysEqual(valueUpdates, expectedValues);
     }
   );
@@ -250,7 +260,7 @@ describe("Variable Slider", () => {
       expect(valueUpdates.length).toBe(0);
 
       act(() => el.slider.focus());
-      await userWaits(100);
+      await userWaits(100, { useFake: true });
       await user.keyboard(`{${key}}`);
       expect(increment).toBeGreaterThan(0);
       expect(typeof valueUpdates[0]).toBe("number");
@@ -276,7 +286,7 @@ describe("Variable Slider", () => {
       };
       const { el, valueUpdates } = await setupTest(props);
       await user.click(el.btnToggle);
-      await userWaits(timeout * 1000);
+      await userWaits(timeout * 1000, { useFake: true });
       await user.click(el.btnToggle);
 
       expectSliderArraysEqual(valueUpdates, expectedValues);
