5b65074602a6e414c35244e06163fe5c50cc17b4
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-10-04 20:01:24 -0400

    update mathlive

--- client/src/util/components/MathLive/MathField.tsx
+++ client/src/util/components/MathLive/MathField.tsx
@@ -43,7 +43,6 @@ const MathFieldForwardRef = (
     ...others
   } = props;
   const [mf, setMf] = useState<MathfieldElement | null>(null);
-  const cancelNextEvent = useRef(false);
 
   useEffect(() => {
     if (!mf) return;
@@ -56,7 +55,6 @@ const MathFieldForwardRef = (
     if (!mf) return;
     const mfValue = mf.getValue();
     if (mfValue !== children) {
-      cancelNextEvent.current = true;
       mf.setValue(children);
       /**
        * In an empty mathfield:
@@ -72,25 +70,10 @@ const MathFieldForwardRef = (
     }
   });
 
-  const handleInput: React.ChangeEventHandler<MathfieldElement> = useCallback(
-    (e) => {
-      if (!cancelNextEvent.current) {
-        onChange?.(e);
-      }
-      cancelNextEvent.current = false;
-    },
-    [onChange]
-  );
-
   useImperativeHandle(ref, () => mf);
 
   return (
-    <math-field
-      {...others}
-      class={className}
-      onInput={handleInput}
-      ref={setMf}
-    />
+    <math-field {...others} class={className} onInput={onChange} ref={setMf} />
   );
 };
 
