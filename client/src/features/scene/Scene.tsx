a7cc6141fa83e574d5f6f7c5da25228e1b190c3e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-12-11 22:23:54 -0500

    add mathbox, center mathbox

--- client/src/features/scene/Scene.tsx
+++ client/src/features/scene/Scene.tsx
@@ -1,20 +1,36 @@
 import mergeClassNames from "classnames";
 import React from "react";
+import * as MB from "mathbox-react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 type Props = {
   className?: string;
 };
 
-const Scene: React.FC<Props> = (props) => (
-  // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-  <div
-    className={mergeClassNames(props.className)}
-    style={{ backgroundColor: "aquamarine", width: "100%", height: "100%" }}
-    onClick={() => {
-      // eslint-disable-next-line no-alert
-      alert("clicked");
-    }}
-  />
-);
+const mathboxOptions = {
+  plugins: ["core", "controls", "cursor"],
+  controls: {
+    klass: OrbitControls,
+  },
+};
+
+const Scene: React.FC<Props> = (props) => {
+  const [container, setContainer] = React.useState<HTMLDivElement | null>(null);
+  return (
+    <div
+      className={mergeClassNames(props.className)}
+      style={{ width: "100%", height: "100%" }}
+      ref={setContainer}
+    >
+      {container && (
+        <MB.Mathbox container={container} options={mathboxOptions}>
+          <MB.Cartesian>
+            <MB.Grid axes="xz" />
+          </MB.Cartesian>
+        </MB.Mathbox>
+      )}
+    </div>
+  );
+};
 
 export default Scene;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,10 +7,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient } from "@tanstack/react-query";
+import * as mb from "mathbox";
 import { theme } from "./mui";
 
 import AppRoutes from "./app";
 import { getStore } from "./store/store";
+
+window.mb = mb;
 
 const prepare = async () => {
   if (import.meta.env.VITE_USE_MSW) {
