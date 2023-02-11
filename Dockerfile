49b75d0de0e530ab8f05cfa4b85d42ebbfb73e53
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-02-11 08:16:45 -0500

    database

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
-FROM python:3.11.2-slim
+FROM python:3.11.2
 
 WORKDIR /src
+COPY /webserver/ /src/
+
+# Install packages and add repo needed for postgres 9.6
+COPY /webserver/apt.txt /tmp/apt.txt
+RUN apt-get update
+RUN apt-get install -y $(grep -vE "^\s*#" apt.txt  | tr "\n" " ")
 
 ENV PIP_DEFAULT_TIMEOUT=100 \
     PIP_DISABLE_PIP_VERSION_CHECK=1 \
@@ -8,7 +14,6 @@ ENV PIP_DEFAULT_TIMEOUT=100 \
     POETRY_VERSION=1.3.1
 
 RUN pip install "poetry==$POETRY_VERSION"
-COPY /webserver/ /src/
 
 RUN poetry config virtualenvs.create false && \
     poetry install --no-root && \
--- webserver/main/settings.py
+++ webserver/main/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,11 +76,10 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES =  {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
