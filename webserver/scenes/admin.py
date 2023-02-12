f0025185aa889f5cdcb7267e12ae9b155070affe
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-02-12 13:06:02 -0500

    add legacyscene model

---
+++ webserver/scenes/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+import scenes.models as models
+
+admin.site.register(models.LegacyScene)
---
+++ webserver/scenes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScenesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scenes'
