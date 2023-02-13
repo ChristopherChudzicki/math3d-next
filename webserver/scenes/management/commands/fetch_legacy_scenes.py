5cf1ba4a0979d517493ddff3c919b5f3ca03e894
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-02-13 08:53:53 -0500

    fetch legacy data

---
+++ webserver/scenes/management/commands/fetch_legacy_scenes.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+import psycopg2
+from psycopg2.extras import DictCursor
+from psycopg2 import sql
+import os
+import tqdm
+from scenes.models import LegacyScene
+
+class Command(BaseCommand):
+    help = '''Fetches legacy scenes from the database specified by the
+    LEGACY_DATABASE_URL environment variable and saves as LegacyScene
+    objects'''
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-l",
+            "--limit",
+            dest='limit',
+            type=int,
+            help='Limit the number of scenes fetched'
+        )
+
+    def get_cursor(self):
+        conn = psycopg2.connect(os.environ['LEGACY_DATABASE_URL'], cursor_factory=DictCursor)
+        return conn.cursor()
+
+    def handle(self, *args, **options):
+        limit = options['limit']
+        cursor = self.get_cursor()
+        query = sql.SQL(f'''
+        SELECT url_key, dehydrated, times_accessed, last_accessed
+        FROM graphs LIMIT {limit};
+        ''').format(limit=limit)if limit else sql.SQL('SELECT * FROM graphs;')
+        cursor.execute(query)
+        
+        for scene in tqdm.tqdm(cursor.fetchall()):
+            LegacyScene.objects.update_or_create(
+                key=scene['url_key'],
+                defaults={
+                    'dehydrated': scene['dehydrated'],
+                    'times_accessed': scene['times_accessed'],
+                    'last_accessed': scene['last_accessed']
+                }
+            )
--- webserver/scenes/models.py
+++ webserver/scenes/models.py
@@ -5,7 +5,7 @@ class LegacyScene(models.Model):
     A scene that was created in the legacy system.
     """
 
-    key = models.CharField(max_length=30)
+    key = models.CharField(max_length=80, unique=True)
     times_accessed = models.IntegerField(default=0)
     last_accessed = models.DateTimeField(auto_now=True)
     dehydrated = models.JSONField()
