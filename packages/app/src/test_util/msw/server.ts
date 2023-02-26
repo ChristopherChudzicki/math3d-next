d413cd7f1b51604edbc5069cf5e4c6ac25949856
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-02-25 20:01:30 -0500

    use node18

--- packages/app/src/test_util/msw/server.ts
+++ packages/app/src/test_util/msw/server.ts
@@ -1,6 +1,7 @@
 import { setupServer } from "msw/node";
 import { handlers } from "./handlers";
 
+console.log("Running SETUP SERVER!")
 /**
  * A request-mocking server for use in NodeJS, e.g., integration tests.
  */
