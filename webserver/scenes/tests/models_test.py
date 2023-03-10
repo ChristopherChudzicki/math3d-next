0b2a46ddef97aa5776b83da9be2b319500848acb
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-03-09 23:38:44 -0500

    allow testing db access

--- webserver/scenes/tests/models_test.py
+++ webserver/scenes/tests/models_test.py
@@ -1,12 +1,9 @@
-from unittest import mock
-
 import pytest
 from django.db import models
 
 from scenes.models import Scene
 
 
-@mock.patch.object(models.Model, "save")
 @pytest.mark.django_db
 def test_my_user(save_mock):
     scene = Scene(
@@ -17,4 +14,3 @@ def test_my_user(save_mock):
         created_at="2021-01-01 00:00:00",
     )
     scene.save()
-    # assert save_mock.called
