2287fff8b8764ec8f750380cec071d499839985e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-03-18 19:55:14 -0400

    update mathjs (#444)

--- packages/mathjs-utils/src/getDependencies.ts
+++ packages/mathjs-utils/src/getDependencies.ts
@@ -1,17 +1,22 @@
 import type * as math from "mathjs";
+import {
+  isAssignmentNode,
+  isFunctionAssignmentNode,
+  isSymbolNode,
+} from "mathjs";
 
 // eslint-disable-next-line no-underscore-dangle
 const _getDependencies = (
   node: math.MathNode,
   omit: Set<string> = new Set([])
 ): Set<string> => {
-  if (node.type === "AssignmentNode") return _getDependencies(node.value);
-  if (node.type === "FunctionAssignmentNode") {
+  if (isAssignmentNode(node)) return _getDependencies(node.value);
+  if (isFunctionAssignmentNode(node)) {
     return _getDependencies(node.expr, new Set(node.params));
   }
   const dependencies = new Set<string>();
   node.traverse((n) => {
-    if (n.type === "SymbolNode" && !omit.has(n.name)) {
+    if (isSymbolNode(n) && !omit.has(n.name)) {
       dependencies.add(n.name);
     }
   });
