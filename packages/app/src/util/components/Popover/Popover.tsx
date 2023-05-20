08023c2f7657b9b58f9f71be7bd7d9e77a73d4af
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-05-20 09:05:24 -0400

    update mui; make poppers trap focus

--- packages/app/src/util/components/Popover/Popover.tsx
+++ packages/app/src/util/components/Popover/Popover.tsx
@@ -3,6 +3,8 @@ import MuiPopper, { PopperProps } from "@mui/material/Popper";
 import { styled, useTheme } from "@mui/material/styles";
 import Fade from "@mui/material/Fade";
 import classNames from "classnames";
+import FocusTrap from "@mui/base/FocusTrap";
+import { TransitionProps } from "@mui/material/transitions";
 import usePointerAway from "./usePointerAway";
 import style from "./Popover.module.css";
 
@@ -160,7 +162,9 @@ const Popover: React.FC<PopoverProps> = ({
       className={classNames(style.container, className)}
     >
       <Arrow ref={setArrowEl} className="MuiPopper-arrow" />
-      {children}
+      <FocusTrap open={visible}>
+        <div tabIndex={-1}>{children}</div>
+      </FocusTrap>
     </Component>
   );
   const theme = useTheme();
@@ -177,9 +181,11 @@ const Popover: React.FC<PopoverProps> = ({
         placement={placement}
       >
         {transition
-          ? ({ TransitionProps }) => (
-              <Fade {...TransitionProps} timeout={duration}>
-                {contents}
+          ? (props: { TransitionProps: TransitionProps }) => (
+              <Fade {...props.TransitionProps} timeout={duration}>
+                <div>
+                  <FocusTrap open={visible}>{contents}</FocusTrap>
+                </div>
               </Fade>
             )
           : contents}
