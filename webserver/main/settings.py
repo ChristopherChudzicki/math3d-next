91e7977230aa3ca776b2a95f799970c21f871d74
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-06-25 19:35:29 -0400

    remove staticfiles app

--- webserver/main/settings.py
+++ webserver/main/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "rest_framework",
     "main",
     "scenes",
