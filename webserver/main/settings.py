af94faf6baa7648b8055ae04b10d8074d45f6438
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-06-25 19:41:28 -0400

    specify runtime

--- webserver/main/settings.py
+++ webserver/main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    # "django.contrib.staticfiles",
     "rest_framework",
     "main",
     "scenes",
