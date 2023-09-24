2dce99f902f6212499fab404a49b556b06507381
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-09-24 09:35:44 -0400

    add some camera tests

--- packages/app/src/playwright/tests-e2e/util.ts
+++ packages/app/src/playwright/tests-e2e/util.ts
@@ -1,11 +1,14 @@
 import { MathItem } from "@math3d/mathitem-configs";
 import type { Locator, Page } from "@playwright/test";
+import { expect } from "@playwright/test";
 import type { Mathfield } from "mathlive";
 /**
  * Importing from "@/test_util" includes some TSX which Playwright can't handle,
  * so need to import the TS-only files directly
  */
 import { makeItem } from "@/test_util/factories";
+import { Scene } from "@/types";
+import type { MathboxSelection } from "mathbox";
 
 const getItemForm = (page: Page, item: MathItem): Locator => {
   return page.locator(
@@ -29,4 +32,24 @@ const getLatex = (e: HTMLElement | SVGElement) => {
   return mf.getValue();
 };
 
-export { getItemForm, makeItem, getLatex };
+const mockScene = (page: Page, scene: Scene) => {
+  return page.route(`*/**/v0/scenes/${scene.key}/`, async (route) => {
+    await route.fulfill({ json: scene });
+  });
+};
+
+/**
+ * Resolves when mathbox has rendered.
+ */
+const whenMathboxRendered = (page: Page) =>
+  expect(async () => {
+    const exists = await page.evaluate(
+      () =>
+        // @ts-expect-error We assign mathbox to window, but leaving it off
+        // the type definitions to discourage use outside of debugging.
+        !!window.mathbox
+    );
+    expect(exists).toBe(true);
+  }).toPass();
+
+export { getItemForm, makeItem, getLatex, mockScene, whenMathboxRendered };
