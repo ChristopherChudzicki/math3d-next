04f60ccaebfbee53fc73dd6b5b0149a945ec2d94
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-11-16 20:34:50 -0500

    Generate OpenAPI & TS Client

---
+++ packages/api/src/paceholder.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "vitest";
+
+test("Placeholder", () => {
+  expect(1).toBe(1);
+});
---
+++ scripts/generate_openapi.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+if [ -z "$(which docker)" ]; then
+	echo "Error: Docker must be available in order to run this script"
+	exit 1
+fi
+
+##################################################
+# Generate OpenAPI Schema
+##################################################
+docker compose run --rm webserver \
+	./manage.py spectacular \
+	--urlconf main.urls \
+	--file ./openapi.yaml \
+	--validate
+
+##################################################
+# Generate API Client
+##################################################
+
+GENERATOR_VERSION=v7.1.0
+
+docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli:${GENERATOR_VERSION} \
+	generate \
+	-i /local/webserver/openapi.yaml \
+	-g typescript-axios \
+	-o /local/packages/api/src/generated \
+	--ignore-file-override /local/packages/api/.openapi-generator-ignore \
+	--additional-properties=useSingleRequestParameter=true,paramNaming=original
+
+# We expect pre-commit to exit with a non-zero status since it is reformatting
+# the generated code.
+git ls-files packages/api/src/generated | xargs pre-commit run --files openapi.yaml ||
+	echo "OpenAPI generation complete."
