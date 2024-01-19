121c055cbcd3119a0ef56a1e2d3a6b5e9228ba29
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-01-19 09:23:44 -0500

    seed e2e test data

---
+++ webserver/main/management/commands/seed_test_data.py
@@ -0,0 +1,55 @@
+import os
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from scenes.models import Scene
+import environ
+import json
+
+
+env = environ.Env(
+    TEST_USER_1_EMAIL=(str, ""),
+    TEST_USER_1_PASSWORD=(str, ""),
+    TEST_USER_2_EMAIL=(str, ""),
+    TEST_USER_2_PASSWORD=(str, ""),
+)
+
+User = get_user_model()
+
+
+def create_test_user(email: str, password: str, public_nickname: str):
+    user, _ = User.objects.get_or_create(email=email)
+    user.is_active = True
+    user.public_nickname = public_nickname
+    user.set_password(password)
+    user.save()
+    return user
+
+
+TEST_SCENE_COUNT = 100
+
+
+class Command(BaseCommand):
+    help = """Seed test data for e2e tests"""
+
+    def handle(self, *args, **options):
+        user_1 = create_test_user(
+            email=env("TEST_USER_1_EMAIL"),
+            password=env("TEST_USER_1_PASSWORD"),
+            public_nickname="Static Test User",
+        )
+        create_test_user(
+            email=env("TEST_USER_2_EMAIL"),
+            password=env("TEST_USER_2_PASSWORD"),
+            public_nickname="Dynamic Test User",
+        )
+
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, "./test_scene.json")
+        with open(filename) as f:
+            test_scene = json.load(f)
+
+        for j in range(TEST_SCENE_COUNT):
+            title = title = f"Test Scene {j}"
+            s = Scene.objects.get(title=title, author=user_1)
+            s.items = test_scene["items"]
+            s.item_order = test_scene["itemOrder"]
