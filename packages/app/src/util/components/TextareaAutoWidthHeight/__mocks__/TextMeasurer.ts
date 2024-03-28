eca557a8ad4c86d310350e69ae166136e717863f
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-03-27 21:36:59 -0400

    update ts

--- packages/app/src/util/components/TextareaAutoWidthHeight/__mocks__/TextMeasurer.ts
+++ packages/app/src/util/components/TextareaAutoWidthHeight/__mocks__/TextMeasurer.ts
@@ -6,8 +6,13 @@ const fakeTextMetrics = (): TextMetrics => ({
   actualBoundingBoxDescent: 1,
   actualBoundingBoxLeft: 1,
   actualBoundingBoxRight: 1,
+  alphabeticBaseline: 1,
+  emHeightAscent: 1,
+  emHeightDescent: 1,
   fontBoundingBoxAscent: 1,
   fontBoundingBoxDescent: 1,
+  hangingBaseline: 1,
+  ideographicBaseline: 1,
   width: 1,
 });
 
