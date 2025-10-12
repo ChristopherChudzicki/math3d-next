0f7bc673c77a0e7617101dba0e4c209cb4940078
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-10-12 10:19:18 -0400

    more css fixes (#1048)
    * more css fixes
    
    * lint

--- packages/app/src/features/sceneControls/mathItems/forms/VariableSlider/SliderControls.tsx
+++ packages/app/src/features/sceneControls/mathItems/forms/VariableSlider/SliderControls.tsx
@@ -10,7 +10,6 @@ import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 import RemoveOutlinedIcon from "@mui/icons-material/RemoveOutlined";
 import { assertNotNil } from "@/util";
 import classNames from "classnames";
-import u from "@/util/styles/utils.module.css";
 import styles from "./SliderControls.module.css";
 
 const btnLabels = {
@@ -91,7 +90,7 @@ const SliderControls: React.FC<SliderControlsProps> = ({
   const onStepDown = useCallback(() => onStep(-1), [onStep]);
 
   return (
-    <div className={classNames(u.dFlex, u.alignItemsCenter, className)}>
+    <div className={className}>
       <IconButton
         size="small"
         className={styles.playButton}
