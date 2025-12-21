c095c19b98ff4d7dbe596a5b01f98096f4726a3f
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-12-20 19:49:25 -0500

    remove unused var

--- packages/app/src/vite-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="vite/client" />
-
-declare const __PLAYWRIGHT__: boolean | undefined;
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -73,7 +73,4 @@ export default defineConfig({
       },
     },
   },
-  define: {
-    __PLAYWRIGHT__: process.env.PLAYWRIGHT,
-  },
 });
